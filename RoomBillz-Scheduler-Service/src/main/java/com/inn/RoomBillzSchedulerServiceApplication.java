ab80e3c8f5b0f8d9f5e2a095848f7d2a4845afd4
sarfaraz alam <sarfarazalam2702@gmail.com>
2025-11-09 02:05:14 +0530

    Scheduler - Day-1

---
+++ RoomBillz-Scheduler-Service/src/main/java/com/inn/RoomBillzSchedulerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomBillzSchedulerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomBillzSchedulerServiceApplication.class, args);
+	}
+
+}
